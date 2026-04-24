7e21aed973b4127d38f1355b7746bddcb0a3557f
Rushim7 <mandavars@outlook.com>
2026-04-24 08:11:25 -0400

    first commit

---
+++ python_basics_Rushyanthi.py
@@ -0,0 +1,15 @@
+name = "Snow"
+age = 15
+height_in_meters = 135.50
+is_student = True
+
+print(name ,age ,height_in_meters ,is_student)
+
+print("Name:",name , "| Age:",age , "| Height:",height_in_meters ,"| Student:",is_student)
+
+Age_in_months = 15 * 12
+Age_in_days = 15 * 365
+age_by_7  = 15 % 7
+Age_power_2 = 15 ** 2
+
+print(Age_in_months,Age_in_days,age_by_7,Age_power_2)
---
+++ test2.py
@@ -0,0 +1,8 @@
+name = "Snow"
+age = 15
+height_in_meters = 135.50
+is_student = True
+
+print(name ,age ,height_in_meters ,is_student)
+
+print("Name:",name , "| Age:",age , "| Height:",height_in_meters ,"| Student:",is_student)
